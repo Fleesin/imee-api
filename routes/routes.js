ce989dc089a3774e5c1b26ec47d6bb392e3a81be
Fleesin <jean.quintana.123@gmail.com>
2022-05-26 17:47:41 -0500

    routes fix

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
-const Student = require('../database/models/student');
+const User = require('../database/models/student');
 
 router.post('/register', async (req, res) => {
   // Verificar si el correo ya existe.
-  const emailExist = await Student.findOne({ email: req.body.email });
+  const emailExist = await User.findOne({ email: req.body.email });
   if (emailExist) return res.status(400).send({ error: 'El email existe' });
 
   // Hash password
@@ -12,7 +12,7 @@ router.post('/register', async (req, res) => {
   const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
   // Crear un nuevo usuario
-  const user = new Student({
+  const user = new User({
 
     fName: req.body.fName,
     lName: req.body.lName,
@@ -52,7 +52,7 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await Student.findOne({ email });
+  const user = await User.findOne({ email });
   if (!user)
     return res.status(400).send({ error: 'Email or password is wrong' });
 
@@ -66,7 +66,7 @@ router.post('/login', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try {
-    const user = await Student.findById(req.params._id);
+    const user = await User.findById(req.params._id);
     return res.status(200).json(user);
   } catch (error) {
     return res.status(400).json({ error });
