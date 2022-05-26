89f67d08e3fabc20ac4e49408aff394cd253e906
Fleesin <jean.quintana.123@gmail.com>
2022-05-26 17:40:00 -0500

    index fix

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const limiter = rateLimit({
 app.use(limiter);
 
 // Aqui se agregan las rutas.
-app.use('/register', routes);
+app.use('/api', routes);
 
 app.get('/', (req, res) => res.send('⭐'));
 
