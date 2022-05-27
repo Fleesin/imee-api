1b61a95477c09471e1d301c3236282b5f6804258
Fleesin <jean.quintana.123@gmail.com>
2022-05-26 19:55:36 -0500

    db index fix

--- database/index.js
+++ database/index.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
 
-const { log } = console;
-
 mongoose.set('useFindAndModify', false);
 
 module.exports.connect = async () => {
     try{
       await mongoose
       .connect(
-        'mongodb+srv://jeanquintana:paTIGO06XNOeOStx@imee.wdtzgoy.mongodb.net/?retryWrites=true&w=majority'
+        'mongodb+srv://jeanquintana:paTIGO06XNOeOStx@imee.wdtzgoy.mongodb.net/?retryWrites=true&w=majority/test'
       )
       console.log('Conectado a Atlas')
     }catch (error) {
